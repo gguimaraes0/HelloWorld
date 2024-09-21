f48fccaf8398e82f0e46544b5a5103e6aaab9226
Gustavo Guimarães <71523561+gguimaraes0@users.noreply.github.com>
2024-09-21 12:52:23 -0300

    Add files via upload

---
+++ teste-js/scripts.js
@@ -0,0 +1,4 @@
+// Seleciona o botão pelo ID e adiciona um evento de clique
+document.getElementById('botao').addEventListener('click', function() {
+    document.getElementById('titulo').textContent = 'Você clicou no botão!';
+});
